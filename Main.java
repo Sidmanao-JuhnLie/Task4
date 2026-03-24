9d98e3b3fbb8e523b08275320eb2b44dcc5a3b28
RAMIREZ JOHN RALPH <johnralphramirez611@gmail.com>
2026-03-24 14:50:05 +0800

    Implementation of Main

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Main {
+
+    public static void main(String[] args) {
+        HardwareRepository repository = new HardwareRepository();
+        List<Hardware> hardwareList   = repository.getAllHardware();
+
+        printMasterlist(hardwareList);
+        System.out.println();
+        printInventory(hardwareList);
+    }
+}
