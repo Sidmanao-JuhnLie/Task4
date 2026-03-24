107a32ace2a36e3957a04f41841539525d4a19d6
sbcvconstantinoraevin-ai <sbcv.constantinoraevin@gmail.com>
2026-03-24 16:18:56 +0800

    Laptop

--- Phone.java
+++ Phone.java
@@ -1,1 +1,12 @@
+public class Laptop extends Hardware {
+
+    public Laptop(int id, String brand, int spec) {
+        super(id, brand, spec);
+    }
+
+    @Override
+    public String getInterpretation() {
+        return getSpec() + "GB RAM";
+    }
+}
 
